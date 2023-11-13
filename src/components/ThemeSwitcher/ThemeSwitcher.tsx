70eaae9372b5cf6f260d8447616b13dd7643f742
Max <ppmax@bk.ru>
2023-11-13 19:42:36 +0300

    Theme switcher finished

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,9 +1,22 @@
+import { ReactComponent as MoonIcon } from 'assets/icon-moon.svg';
+import { ReactComponent as SunIcon } from 'assets/icon-sun.svg';
+import { useEffect, useState } from 'react';
 import styles from './ThemeSwitcher.module.scss';
 
-interface ThemeSwitcherProps { }
+const ThemeSwitcher = () => {
+  const [isDark, setDark] = useState(false);
+  const themeText = isDark ? 'Light' : 'Dark';
+  const ThemeIcon = isDark ? SunIcon : MoonIcon;
 
-export const ThemeSwitcher = ({ }: ThemeSwitcherProps) => (
-  <div className={styles.ThemeSwitcher} data-testid="ThemeSwitcher">
-    ThemeSwitcher Component
-  </div>
-);
+  useEffect(() => {
+    document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
+  }, [isDark]);
+
+  return (
+    <button className={styles.switcher} type="button" onClick={() => setDark(!isDark)}>
+      <span>{themeText}</span>
+      <ThemeIcon className={styles.icon} />
+    </button>
+  );
+};
+export default ThemeSwitcher;
