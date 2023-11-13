108ab14ebde73e2a64c6d1e072f4804293bb95e5
Max <ppmax@bk.ru>
2023-11-13 19:42:23 +0300

    Main components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-function App() {
-  return (
-    <>
-      
-    </>
-  );
-}
+import Container from 'components/Container/Container';
+import Header from 'components/Header/Header';
+import Search from 'components/Search/Search';
+
+const App = () => (
+  <Container>
+    <Header />
+    <Search hasError onSubmit={() => {}} />
+  </Container>
+);
 
 export default App;
