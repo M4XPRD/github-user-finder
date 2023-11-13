61cef6cfa0a0a2a6fa74daa2f8c5e6ab4de4d358
Max <ppmax@bk.ru>
2023-11-13 19:41:22 +0300

    Button component added

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+  children: string,
+  onClick?: React.MouseEventHandler<HTMLButtonElement>,
+}
+
+const Button = ({ children, onClick }: ButtonProps) => (
+  <button className={styles.button} type="submit" onClick={onClick}>
+    {children}
+  </button>
+);
+
+export default Button;
