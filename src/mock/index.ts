6414c7c5d368749bd24395cd16cb333cffe0fabc
Max <ppmax@bk.ru>
2023-11-14 16:44:34 +0300

    Default user added

---
+++ src/mock/index.ts
@@ -0,0 +1,19 @@
+import { LocalGithubUser } from 'types';
+
+const defaultUser: LocalGithubUser = {
+  login: 'M4XPRD',
+  id: 86636158,
+  avatar: 'https://avatars.githubusercontent.com/u/86636158?v=4',
+  name: 'Max',
+  company: 'Open for work',
+  blog: '',
+  location: 'Russia, Saint-Petersburg',
+  bio: 'React Developer',
+  twitter: '',
+  repos: 23,
+  followers: 3,
+  following: 7,
+  createdAt: '2021-06-29T00:45:10Z',
+};
+
+export default defaultUser;
